f7d401a8fa8dde991ccda1e424ea3d8b1c34ca48
Sasha <example@gmail.com>
2023-04-11 17:27:16 +0300

    did a validation

--- src/js/script.js
+++ src/js/script.js
@@ -58,3 +58,29 @@ $('.button_mini').each(function(i){
 		$('.overlay, #order').fadeIn();
 	})
 });
+
+function validateForms(form){
+	$(form).validate({
+		rules: {
+			name: "required",
+			phone: "required",
+			email: {
+				required: true,
+				email: true
+			}
+		},
+		messages: {
+			name: "Пожалуйста, введите свое имя",
+			phone: "Пожалуйста, введите свой номер телефона",
+			email: {
+			  required: "Пожалуйста, введите свою почту",
+			  email: "Неправильно введен адрес почты"
+			}
+		  }
+	});
+};
+
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
